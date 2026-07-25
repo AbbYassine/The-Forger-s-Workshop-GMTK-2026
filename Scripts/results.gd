8c33e811ba498680786fc0ff28a4e7f977494110
ABBOURA Ahmed Yassine <abboura0006@gmail.com>
2026-07-25 19:22:32 +0100

    Reapply "fininshed all the code part"
    This reverts commit 7b66a37ff1cfddb151f54d9f46f726b822b5447c.

--- Scripts/results.gd
+++ Scripts/results.gd
@@ -5,6 +5,8 @@ extends Control
 @onready var progress_bar: ProgressBar = $ProgressBar
 @onready var next_button: Button = $NextButton
 
+var total_paintings: int = 0
+
 func _ready() -> void:
 
 	var player_texture = ImageTexture.create_from_image(GameData.player_image)
@@ -25,12 +27,16 @@ func animate_accuracy() -> void:
 	var target = GameData.accuracy_score
 	progress_bar.value = 100
 	var tween = create_tween()
-	tween.tween_method(_update_accuracy_display, 0.0, target, 1.5)
+	tween.set_parallel(true)
+	tween.tween_method(_update_accuracy_label, 0.0, target, 1.5)
+	tween.tween_property(progress_bar, "value", target, 1.5)
 
-func _update_accuracy_display(value):
+func _update_accuracy_label(value: float) -> void:
 	accuracy_label.text = str(int(value)) + "%"
-	progress_bar.value = 100.0 - value
 	
 
 func _on_next_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/sub_viewport_container.tscn")
+	if GameData.is_run_complete():
+		get_tree().change_scene_to_file("res://scenes/end_screen.tscn")
+	else:
+		get_tree().change_scene_to_file("res://Scenes/sub_viewport_container.tscn")
